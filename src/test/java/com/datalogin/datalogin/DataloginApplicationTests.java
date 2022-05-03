3030d08ff58ea73a52a666ef4abb4547ddbe7f90
KrishnaMoorthy <34851933+KitchaKrish@users.noreply.github.com>
2022-05-03 18:24:37 +0530

    Update DataloginApplicationTests.java

--- src/test/java/com/datalogin/datalogin/DataloginApplicationTests.java
+++ src/test/java/com/datalogin/datalogin/DataloginApplicationTests.java
@@ -12,5 +12,30 @@ public class DataloginApplicationTests {
     @Test
     public void contextLoads() {
     }
+private static void adduser(String userOne, String role) {
+		
+		RoleDomain domain = sdsd.get(role);
 
+		if (domain!=null) {
+			RoleDomain domain1 = sdsd.get(domain.getReportRole());
+
+			if (domain1 != null) {
+				List<String> users = domain1.getUser();
+				if (users == null) {
+					users = new ArrayList<>();
+				}
+
+				if (!role.equalsIgnoreCase(domain1.getReportRole())) {
+
+					users.add(userOne);
+					domain1.setUser(users);
+					sdsd.put(role, domain);
+					adduser(userOne, domain.getReportRole());
+				}
+			}
+
+		}
+
+	}
+	
 }
